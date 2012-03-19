1215914886210c9cf020946bcee32b2b7b4660a2
Kim Holmes <zorianak@me.com>
2012-03-18 21:49:55 -0700

    Initial commit.  Netbeans setup.  Systems ready, check, go!

---
+++ JavaEuler/src/javaeuler/JavaEuler.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaeuler;
+
+/**
+ *
+ * @author ohai
+ */
+public class JavaEuler {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
