dbde962aedc6775034367866699dd61d18d57aea
Kim Holmes <zorianak@me.com>
2012-03-25 21:19:33 -0700

    File for ProblemTwo added.

---
+++ JavaEuler/src/javaeuler/ProblemTwo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaeuler;
+
+/**
+ *
+ * @author LoLBRB
+ */
+public class ProblemTwo extends Problem {
+    public static void ProbTwo(int problemNumber)
+    {
+        
+    }
+}
